3a68bc1fcb0e3db9f09bbbcf4d72d3d6e3ef5014
Crowdbotics <team@crowdbotics.com>
2021-11-01 16:58:54 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "easy_habit_tracker_31794",
+  url: "https://easy_habit_tracker_31794.botics.co",
+  api: "https://easy_habit_tracker_31794.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
